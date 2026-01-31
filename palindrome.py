484335838fb676324446164206fed71f4de2a1c6
fahiq7 <muhammadfahique@gmail.com>
2026-01-31 09:47:32 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: bool
+        """
+        if not head or not head.next:
+            return True
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        prev = None
+        curr = slow
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        left, right = head, prev
+        while right: 
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
